6a3585a771a17fcd53af558265bf4d05960f9bda
Alfas Salim <75639073+Alfas1511@users.noreply.github.com>
2025-08-20 19:40:03 +0000

    changes

--- myApp.js
+++ myApp.js
@@ -1,9 +1,19 @@
+require('dotenv').config();
 let express = require("express");
 let app = express();
 app.use('/public', express.static(__dirname + "/public"));
 
-app.get("/json", function (req, res) {
-  res.json({ message: "Hello json" });
+// app.get("/json", function (req, res) {
+//   if(process.env.MESSAGE_STYLE === "uppercase"){
+//     res.json({ message: "HELLO JSON" });
+//   }else{
+//     res.json({ message: "Hello json" });
+//   }
+// });
+
+app.get("/json", function (req, res, next){
+  console.log(req.method, req.path, "-", req.ip);
+  next();
 });
 
 
