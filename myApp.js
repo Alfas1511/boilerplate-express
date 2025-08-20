76ef43ff650dfec6bc78c1a889453e7525b08623
Alfas Salim <75639073+Alfas1511@users.noreply.github.com>
2025-08-20 14:04:10 +0000

    Endpoint and json response

--- myApp.js
+++ myApp.js
@@ -2,6 +2,11 @@ let express = require("express");
 let app = express();
 app.use('/public', express.static(__dirname + "/public"));
 
+app.get("/json", function (req, res) {
+  res.json({ message: "Hello json" });
+});
+
+
 // console.log("Hello World");
 app.get("/", function (req, res) {
   // res.send("Hello Express");
